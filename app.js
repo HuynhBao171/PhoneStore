37eaea16b158f735fe0b69bc239e0a00185b0c9b
HuynhBao171 <bqhuynh171@gmail.com>
2024-04-19 23:40:07 +0700

    final

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 
 var app = express();
+var cors = require("cors");
+// // Use CORS middleware
+// const corsOptions = {
+//   origin: "http://127.0.0.1:5500/",
+//   optionsSuccessStatus: 200,
+// };
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
